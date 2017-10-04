06f08f3508007ee793fd61df62d1fba18beede0e
Bernard Strong <bstrong71@gmail.com>
2017-10-04 14:00:04 -0400

    game functionality complete

--- public/quiz.js
+++ public/quiz.js
@@ -1,4 +1,30 @@
 // show answer by changing color of text
+
+let score = 0;
+let total = 0;
+let quizScore = document.querySelector(".score");
+let quizCard = document.getElementsByClassName("eachQuizCard");
+let totalCards = quizCard.length;
+
+function gotItRight() {
+  score++;
+  total++;
+  if (total !== totalCards) {
+    quizScore.innerHTML = `Your Score: ${score} / ${total}`
+  } else {
+    quizScore.innerHTML = `Thanks for Playing! You Got: ${score} / ${total}!`
+  }
+}
+
+function gotItWrong() {
+  total++;
+  if (total !== totalCards) {
+    quizScore.innerHTML = `Your Score: ${score} / ${total}`
+  } else {
+    quizScore.innerHTML = `Thanks for Playing! You Got: ${score} / ${total}!`
+  }
+}
+
 function showAnswer(button) {
   button.previousElementSibling.style.color = "blue";
 }
